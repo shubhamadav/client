21a3523e83fc2d5d75a641fc76643f6a51cec7e3
shubhamadav <shubhamyad100@gmail.com>
2023-09-20 20:57:31 +0530

    Chat App real time

--- src/App.js
+++ src/App.js
@@ -2,39 +2,34 @@ import React from 'react';
 import Form from './module/Form'; // Assuming the path to Form.js is correct
 import './App.css';
 import Dashboard from './module/dashboard'; // Assuming the path to Dashboard.js is correct
-import {Routes , Route , Navigate} from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 
-const ProtectedRoute = ({children})=>{
-  const isLoggedIn = localStorage.getItem('user.token')!=null || true
+const ProtectedRoute = ({ children, auth = false }) => {
+  const location = useLocation();
+  const isLoggedIn = localStorage.getItem('user:token') !== null; // Use the correct localStorage key
 
-  if(!isLoggedIn){
-  return <Navigate to={'/users/sign_in'}/>
-  }else if(isLoggedIn && ['/users/sign_in'&& '/users/sign_up'].includes(window.location.pathname)){
-    return <Navigate to={'/'}/>
+  if (!isLoggedIn && auth) {
+    return <Navigate to="/users/sign_in" />;
+  } else if (isLoggedIn && ['/users/sign_in', '/users/sign_up'].includes(location.pathname)) {
+    return <Navigate to="/" />;
   }
-  return children
-}
+  return children;
+};
 
 function App() {
   return (
-     <Routes>
-          <Route  path='/' element={
-             <ProtectedRoute>
-                <Dashboard/>  
-             </ProtectedRoute>
-          }/>
-         <Route  path='/users/sign_in' element={<ProtectedRoute>
-          <Form isSignInPage={true}/>
-         </ProtectedRoute>}/>
-         <Route  path='/users/sign_up' element={<ProtectedRoute>
-          <Form isSignInPage={false}/>
-         </ProtectedRoute>}/>
-     </Routes>
-
-    //<div className="bg-[#e1edff] h-screen flex justify-center items-center">
-      //{/* <Form /> */}
-      //<Dashboard />
-    //</div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute auth={true}>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/users/sign_in" element={<Form isSignInPage={true} />} />
+      <Route path="/users/sign_up" element={<Form isSignInPage={false} />} />
+    </Routes>
   );
 }
 
